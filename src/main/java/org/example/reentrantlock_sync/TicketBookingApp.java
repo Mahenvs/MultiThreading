0faae1bedb50b0cd9419222a2cebdb958943cb0b
mahenv <mahendrauppari044@gmail.com>
2025-08-03 12:15:31 -0500

    Add thread-safe ticket booking using ReentrantLock

--- src/main/java/org/example/reentrantlock_sync/TicketBookingApp.java
+++ src/main/java/org/example/reentrantlock_sync/TicketBookingApp.java
@@ -1,32 +1,29 @@
 package org.example.reentrantlock_sync;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TicketBookingApp {
     public static void main(String[] args) throws InterruptedException {
         TicketCounter counter = new TicketCounter();
-        String[] users = {"mahe","manish","venu","sai","vam","mahi","mahesh"};
-        String[] newUsers = {"Priya","Aarohi","Abhigna","Ankitha","Aishwarya","Lavanya","Manisha"};
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for(String user: users){
+        String[] users = {"mahe", "manish", "Priya", "Aarohi", "Abhigna", "Ankitha", "venu", "sai", "vam", "mahi"};
+
+        List<Thread> threads = new ArrayList<>();
+        for (String user : users) {
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
                     counter.bookTicket(user);
                 }
-            }
-        });
+            });
+            t.start();
+            threads.add(t);
+        }
 
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for(String user: newUsers){
-                    counter.bookTicket(user);
-                }
-            }
-        });
-        t1.start();
-        t2.start();
-
-        t1.join();
-        t2.join();
-
+//      Without join(), your main program can terminate early, causing unfinished work or incomplete console output.
+//      With join(), you ensure all concurrent tasks finish before main exits — this is important for correctness and predictable program behavior.
+        for (Thread t : threads) {
+            t.join();
+        }
     }
 }
--- src/main/java/org/example/reentrantlock_sync/TicketCounter.java
+++ src/main/java/org/example/reentrantlock_sync/TicketCounter.java
@@ -1,9 +1,12 @@
 package org.example.reentrantlock_sync;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TicketCounter {
     private int ticketsAvailable = 5;
-
+    private final ReentrantLock lock = new ReentrantLock();
     public void bookTicket(String user){
+        lock.lock(); //Acquire lock
         try{
             if(ticketsAvailable > 0){
                 System.out.println(user+" is booking a ticket");
@@ -16,6 +19,8 @@ public class TicketCounter {
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+        }finally {
+            lock.unlock();//Always release lock
         }
 
     }
