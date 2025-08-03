a08986f3e70ad81946f15ce296f47f8201c3b011
mahenv <mahendrauppari044@gmail.com>
2025-08-03 13:45:04 -0500

    Using Try Lock with Reentrant

---
+++ src/main/java/org/example/reentrantlock_sync/TicketBookingApp.java
@@ -0,0 +1,36 @@
+package org.example.reentrantlock_sync;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class TicketBookingApp {
+    public static void main(String[] args) throws InterruptedException {
+        TicketCounter counter = new TicketCounter();
+        String[] users = {"mahe", "manish", "Priya", "Aarohi", "Abhigna", "Ankitha", "venu", "sai", "vam", "mahi"};
+
+        Random random = new Random();
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            String user = users[random.nextInt(users.length)];
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    if(random.nextDouble() < 0.7) {
+                        counter.bookTicket(user);
+                    }else{
+                        counter.cancelTicket(user);
+                    }
+                }
+            });
+            t.start();
+            threads.add(t);
+        }
+
+//      Without join(), your main program can terminate early, causing unfinished work or incomplete console output.
+//      With join(), you ensure all concurrent tasks finish before main exits — this is important for correctness and predictable program behavior.
+        for (Thread t : threads) {
+            t.join();
+        }
+    }
+}
